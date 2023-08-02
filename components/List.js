a03b2cdb88a16595ff12824fddd527a7067d1099
Byron <byron.caos@gmail.com>
2023-08-02 02:06:45 -0500

    list

---
+++ components/List.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const List = ({ op1, op2, op3, op4, op5, op6, op7, op8, op9, op10, op11, op12, op13, op14 }) => {
+    const styleList = {
+
+    };
+    return  <ul style={styleList}>
+        <li>{op1}</li>
+        <li>{op2}</li>
+        <li>{op3}</li>
+        <li>{op4}</li>
+        <li>{op5}</li>
+        <li>{op6}</li>
+        <li>{op7}</li>
+        <li>{op8}</li>
+        <li>{op9}</li>
+        <li>{op10}</li>
+        <li>{op11}</li>
+        <li>{op12}</li>
+        <li>{op13}</li>
+        <li>{op14}</li>
+    </ul>
+};
+
+export default List;
